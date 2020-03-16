e8c75490dbdf03a6f07fb0f596661a746a99a610
badurden <denis.badura@tieto.com>
2020-03-16 13:33:54 +0100

    Color Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -21,6 +21,10 @@ const HomeScreen = ({ navigation }) => {
                 title='Go to Counter Demo'
                 onPress={() => navigation.navigate('Counter')}
             />
+            <Button
+                title='Go to Color Demo'
+                onPress={() => navigation.navigate('Color')}
+            />
 
             {/*          <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
             <Text style={styles.text}>Go to List Demo</Text>
